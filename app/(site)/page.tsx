a3d513a8735eb8724ccc4d1d32827b34bc040770
Oryan Abergel <86202122+Oryanab@users.noreply.github.com>
2024-03-08 17:33:24 +0200

    update final layout

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,12 +1,27 @@
+"use client";
+
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 import React from "react";
 
 const Home = () => {
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
-      <Header>hel</Header>
+      <Header>
+        <div className="mg-2">
+          <h1 className="text-white text-3xl font-semibold">Welcome Back</h1>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+          <ListItem href="liked" name="Liked Songs" image="/images/liked.png" />
+        </div>
+      </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
+        </div>
+        <div>List of songs</div>
+      </div>
     </div>
   );
 };
-
 export default Home;
--- components/Header.tsx
+++ components/Header.tsx
@@ -57,11 +57,22 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
         <div className="flex justify-between items-center gap-x-4">
           <>
             <div>
-              <Button />
+              <Button
+                className="bg-transparent text-neutral-300 font-medium"
+                onClick={() => null}
+              >
+                Sign Up
+              </Button>
+            </div>
+            <div>
+              <Button className="bg-white px-6 py-2" onClick={() => null}>
+                Log in
+              </Button>
             </div>
           </>
         </div>
       </div>
+      {children}
     </div>
   );
 };
