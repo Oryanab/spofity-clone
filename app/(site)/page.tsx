ec3b5db77931802e2768b6aaf2a29eb3c0098abb
Oryan Abergel <86202122+Oryanab@users.noreply.github.com>
2024-03-08 02:43:24 +0200

    update general stracture

---
+++ app/(site)/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>hel</Header>
+    </div>
+  );
+};
+
+export default Home;
